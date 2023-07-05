c6e208b37736d21c0e0baf9f030945afc3f79ea2
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 01:58:16 +0800

    print output

--- TestFan.py
+++ TestFan.py
@@ -1,8 +1,14 @@
+
 # import Fan from Fan file
 from Fan import Fan
+
 # create two fan objects
 class TestFan:
     # first fan
     first_fan = Fan(FAST, 10, "yellow", True)
     # second fan
     second_fan = Fan(MEDIUM, 5, "blue", False)
+
+# print output
+print ("\n")
+first_fan = print("\033[36mFirst Fan: ", "\n\nSpeed: ", first_fan.speed)
