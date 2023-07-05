a218cf445e92862a570c1377e8a9da8caf46cde7
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 00:37:15 +0800

    set defaults

--- Fan.py
+++ Fan.py
@@ -4,20 +4,17 @@
 
 # create fan class
 class Fan:
+
 # create three constants (slow, medium, fast)
     SLOW = 1
     MEDIUM = 1
     FAST = 3
-# speed of the fan (private int data)
-# fan switch (on and off) (private bool data)
-# radius of the fan (private float data)
-# color of the fan (private string data)
 
 # create a constructor (speed, radius, color, switch)
-    def __init__(self, speed, radius, color, switch)
+    def __init__(self, speed = SLOW, radius = 5, color = 'blue', switch = False):
     # behavior (instance methods)
     self.__speed = speed
     self.__radius = radius
     self.__color = color
-    self.__on = on
+    self.__switch = on
 
