ce3f52fb0c57615f66ce98a0ac8ebebb03c45f7b
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 00:21:47 +0800

    pseudocode

--- Fan.py
+++ Fan.py
@@ -1,3 +1,11 @@
 # Name: Pineda, Patricia Anne E.
 # Section: BSCPE 1-5
 # Assignment 9: Abstraction and Encapsulation
+
+# create fan class
+# create three constants (slow, medium, fast)
+# speed of the fan (private int data)
+# fan switch (on and off) (private bool data)
+# radius of the fan (private float data)
+# color of the fan (private string data)
+
