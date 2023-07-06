73c23a75a65c284a1784dc491a8d767962f29b4e
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 09:12:24 +0800

    call acceleration five times

--- CarProgram.py
+++ CarProgram.py
@@ -5,6 +5,8 @@ from Car import Car
 car = Car(2023, "Honda Civic Sedan")
 
 # call acceleration five times
+for i in range(1,6):
+    car.accelerate()
 # get the current speed and display
 # call brake method five times
 # get the current speed and display
