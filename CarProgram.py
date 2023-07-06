985ffa54d6baf684fa95b7540cb2222c3ad23599
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 09:03:00 +0800

    car program pseudocode

---
+++ CarProgram.py
@@ -0,0 +1,6 @@
+# import Car class from Car file
+# create car object
+# call acceleration five times
+# get the current speed and display
+# call brake method five times
+# get the current speed and display
