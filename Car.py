8318b49160694c74b4dbe48311f168d628a7a32d
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 08:38:39 +0800

    car program pseudocode

---
+++ Car.py
@@ -0,0 +1,9 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 9: Abstraction and Encapsulation
+
+# create car class
+# create a constructor (model, make, speed)
+# make accelerate method
+# make brake method
+# get speed method
