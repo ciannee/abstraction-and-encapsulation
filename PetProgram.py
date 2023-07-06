076bb2ce22794637d205dc381d12542608203073
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 11:27:20 +0800

    print output

--- PetProgram.py
+++ PetProgram.py
@@ -17,10 +17,14 @@ pet = Pet(pet_name, pet_animal_type, pet_age)
 user_pet = Pet(pet_name, pet_animal_type, pet_age)
 
 # print output
+print ("\n\n\033[91mYour Pet's Information:")
 
 # display pet name
+name_input = print("\n\033[94mName: ", user_pet.get_name())
 # display pet animal type
+type_input = print("\n\033[36mAnimal Type: ", user_pet.get_animal_type())
 # display pet age
+age_input = print("\n\033[95mAge: ", user_pet.get_age())
 
 
 
