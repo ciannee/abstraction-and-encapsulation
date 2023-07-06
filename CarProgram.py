fdc7c00d19ba3767d6e7d4c0eb3061c9433e40b4
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 09:10:59 +0800

    create car object

--- CarProgram.py
+++ CarProgram.py
@@ -1,6 +1,9 @@
 # import Car class from Car file
 from Car import Car
+
 # create car object
+car = Car(2023, "Honda Civic Sedan")
+
 # call acceleration five times
 # get the current speed and display
 # call brake method five times
