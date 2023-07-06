67f4f047f0c5ecf51af7d3064a1499e3f05a6be8
Patricia Pineda <pepineda28@gmail.com>
2023-07-06 09:34:57 +0800

    pet class pseudocode

---
+++ Pet.py
@@ -0,0 +1,12 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 9: Abstraction and Encapsulation
+
+# create pet class
+# create a constructor
+# set name method
+# set animal type method
+# set age method
+# get name method
+# get animal type method
+# get age method
